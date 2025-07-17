339e3d165714028c8b85ad74f39d3feb51f9399c
Scambyjtech <jasaljazz88@gmail.com>
2025-07-17 22:15:14 +0530

    Updated Gradle wrapper and configs for Flutter 3.19+

--- android/app/src/main/kotlin/com/fintrack/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/fintrack/app/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.fintrack.app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
-
